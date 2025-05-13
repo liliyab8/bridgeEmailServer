496a34e40a9eab3e45185a24aa962b5e707b26e6
liliyab8 <liliya.belogolov@gmail.com>
2025-05-13 16:34:52 +0300

    client added

--- bridge-email-server/src/main/java/com/example/bridge_email_server/factory/impl/YahooFactory.java
+++ bridge-email-server/src/main/java/com/example/bridge_email_server/factory/impl/YahooFactory.java
@@ -3,8 +3,6 @@ package com.example.bridge_email_server.factory.impl;
 import com.example.bridge_email_server.dto.impl.EmailMessageRequest;
 import com.example.bridge_email_server.factory.interfaces.EmailFactory;
 import com.example.bridge_email_server.services.interfaces.EmailSender;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
