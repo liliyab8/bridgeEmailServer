0b011846525b5b1ddb52fc37aefbb025c522c3dc
liliyab8 <liliya.belogolov@gmail.com>
2025-05-13 16:34:07 +0300

    client added

--- bridge-email-server/src/main/java/com/example/bridge_email_server/services/interfaces/EmailSender.java
+++ bridge-email-server/src/main/java/com/example/bridge_email_server/services/interfaces/EmailSender.java
@@ -2,7 +2,7 @@ package com.example.bridge_email_server.services.interfaces;
 
 import com.example.bridge_email_server.dto.abstr.MessageRequest;
 
-interface EmailSender /*implements MessageSender*/ {
+public interface EmailSender /*implements MessageSender*/ {
     public void send(MessageRequest request);
 }
 
